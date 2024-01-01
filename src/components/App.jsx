c7fb5131747132a49a08766559b6e0d0bb25a2dc
deathfluffy <mrandreu454@gmail.com>
2024-01-01 12:40:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
+import  ContactForm  from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import ContactFilter from './ContactFilter/ContactFilter';
 import css from './AppStyled.module.css';
 
-export default function App () {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-
+export default function App  ()  {
   const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
 
   useEffect(() => {
-    const contactsToLocalStorage = localStorage.getItem('contactsToLocalStorage');
-
-    if (contactsToLocalStorage) {
-      try {
-        const parseContactList = JSON.parse(contactsToLocalStorage);
-        setContacts(parseContactList);
-      } catch {
-        setContacts([]);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contactsToLocalStorage', JSON.stringify(contacts));
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
+
 
   const onFormSubmit = ({ id, name, number }) => {
     const contact = { id, name, number };
@@ -45,17 +36,18 @@ export default function App () {
     setContacts(filteredContacts);
   };
 
-  const filterContacts = () => {
+  const onFilterContacts = () => {
     if (filter) {
       return contacts.filter(
         (contact) =>
-          contact.name.includes(filter) || contact.name.toLowerCase().includes(filter)
+          contact.name.includes(filter) ||
+          contact.name.toLowerCase().includes(filter)
       );
     } else {
       return contacts;
     }
   };
-
+  
   return (
     <div className={css.container}>
       <span className={css.TitlePhonebook}>Phonebook</span>
@@ -66,7 +58,7 @@ export default function App () {
         contacts={contacts}
         filter={filter}
         onDelete={onDeleteHandler}
-        filterContacts={filterContacts}
+        filterContacts={onFilterContacts}
       />
     </div>
   );
